1b9a7028e62dbeb16b50f32cc2ed8b6bd096b9f7
Andreas Gropp <starmaerker@googlemail.com>
2019-01-18 13:43:34 +0100

    created schema

--- models/blogposts.js
+++ models/blogposts.js
@@ -1,4 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const blogpostSchema = new Schema();
+const blogpostSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    imagelink: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+var Blogposts = mongoose.model("Blogpost", blogpostSchema);
+
+module.exports = Blogposts;
